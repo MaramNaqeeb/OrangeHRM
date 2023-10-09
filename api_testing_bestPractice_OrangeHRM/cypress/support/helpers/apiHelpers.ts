adc2ae473619d46d2e5ae75808ab1565a87be39c
Maram Naqeeb <maramnaqib1998@gmail.com>
2023-10-09 22:52:17 +0300

    api-bestPractice

---
+++ api_testing_bestPractice_OrangeHRM/cypress/support/helpers/apiHelpers.ts
@@ -0,0 +1,23 @@
+const baseUrl = Cypress.config().baseUrl;
+import { newVacancy, vacancyId,editVacancies,getVacancy,deleteVacancy } from "./payLoadFunctions";
+export const URLs = {
+  vacancy: `${baseUrl}/web/index.php/api/v2/recruitment/vacancies`,
+  editVacancy: `${baseUrl}/web/index.php/api/v2/recruitment/vacancies/8`,
+  getVacancy:`${baseUrl}/web/index.php/api/v2/recruitment/vacancies?limit=50&offset=0&vacancyId=${vacancyId}&sortField=vacancy.name&sortOrder=ASC&model=detailed`,
+  deleteVacancy: `${baseUrl}/web/index.php/api/v2/recruitment/vacancies`,
+};
+export default class apiHelpers {
+  static addVacancy() {
+    cy.vacancy("POST", URLs.vacancy, newVacancy());
+  }
+  static editVacancy() {
+    cy.vacancy("PUT", URLs.editVacancy, editVacancies());
+  }
+  static getVacancy(){
+    cy.vacancy("GET",URLs.getVacancy,getVacancy())
+  }
+  static deleteVacancy() {
+    cy.vacancy("DELETE", URLs.deleteVacancy, deleteVacancy());
+  }
+
+}
---
+++ api_testing_bestPractice_OrangeHRM/cypress/support/projectObject/loginAdmin.ts
@@ -0,0 +1,15 @@
+class loginAdmin {
+  elements = {
+    userName: () => cy.getByCy("Username"),
+    password: () => cy.getByCy("Password"),
+    loginBTN: () => cy.get("[type=submit]"),
+  };
+
+  loginFUNC(userName: string, password: string) {
+    this.elements.userName().type(userName),
+    this.elements.password().type(password),
+    this.elements.loginBTN().click({ force: true });
+  }
+}
+
+export default loginAdmin;
